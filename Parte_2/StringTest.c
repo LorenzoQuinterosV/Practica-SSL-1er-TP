f8a2a7fd46911a5a9c88215369268574d5e8f423
LorenzoQuinterosV <lquinterosvillagra@frba.utn.edu.ar>
2026-05-24 00:50:30 +0000

    hechos los tests

---
+++ Parte_2/StringTest.c
@@ -0,0 +1,47 @@
+#include "String.h"
+#include "String.c"
+#include <assert.h>
+
+int main(){
+
+    assert(IsEmpty("HOLAAA") == false);
+    assert(IsEmpty("") == true);
+
+    assert(GetLength("tamaño 8") == 8);
+    assert(GetLength("tamaño") == 6);
+
+    assert(AreEqual("hola","chau") == false);
+    assert(AreEqual("hola","hola") == true);
+
+    assert(AreEqualNotRecursive("hola","chau") == false);
+    assert(AreEqualNotRecursive("hola","hola") == true);
+
+    assert(AreDecimalDigits("123") == true);
+    assert(AreDecimalDigits("12a3") == false);
+    
+    assert(Contains("holaaa", 'a') == true);
+    assert(Contains("holaaa", 'p') == false);
+
+    assert(ToInteger("-985") == (-985));
+    assert(ToInteger("8") == 8);
+
+    assert(SizeCompare("hola","chau") == 0);
+    assert(SizeCompare("hola","holaa") == -1);
+    assert(SizeCompare("holaa","hola") == 1);
+
+    assert(Concatenate("hola","chau") == "holachau");
+    assert(Concatenate("hola","") == "hola");
+    assert(Concatenate("","chau") == "chau");
+
+    assert(Potenciar("hola", 3) == "holaholahola");
+    assert(Potenciar("hola", 0) == "");
+    assert(Potenciar("", 3) == "");
+
+    assert(revertir("hola") == "aloh");
+    assert(revertir("") == "");
+    assert(revertir(NULL) == NULL);
+
+
+
+
+}
