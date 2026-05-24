d2f90fa9cfa37e43aa25a607e264e7aa77ea58ed
LorenzoQuinterosV <lquinterosvillagra@frba.utn.edu.ar>
2026-05-24 01:13:38 +0000

    longitudes hecho

--- Parte_3/longitudes.c
+++ Parte_3/longitudes.c
@@ -5,6 +5,10 @@ int main(int argc, char *argv[]){
     if(argc == 0){
         printf("No me dieron argumentos");}
 
-        
+    for(int i = 1; i<argc; i++){
+        char* aux = argv[i];
+        int tamaño = (int)GetLenth(aux);
+        printf("el argumento %s tiene tamaño %d \n", aux, tamaño);
+    }    
     return 0;
 }
